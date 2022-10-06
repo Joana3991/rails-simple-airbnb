c6fbee1abf537c62924d20099d0d8cf1dca6ab2e
Joana Areas <jo_areas@hotmail.com>
2022-10-06 01:04:08 -0300

    filter logic

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,8 +2,12 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
-    @query = params[:query]
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @flats = Flat.all
+    end
   end
 
   def new
